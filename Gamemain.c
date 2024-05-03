08e33242a0d83d6328ca5983e44b9c609f1da198
Guilherme Costa <guizeracostasilva@outlook.com>
2024-05-03 16:41:39 -0300

    Avançando com o jogo, movendo matriz, usando struct

--- Gamemain.c
+++ Gamemain.c
@@ -1,23 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include "game.h"
 
-char** mapa;
-int linhas;
-int colunas;
+MAPA m;
 
 void liberamapa(){
     int i;
-    for(i = 0; i < linhas; i++){
-        free(mapa[i]);
+    for(i = 0; i < m.linhas; i++){
+        free(m.matriz[i]);
     }
-    free(mapa);
+    free(m.matriz);
 }
 
 void alocamapa(){
     int i;
-    mapa = malloc(sizeof(char*) * linhas);
-    for(i = 0 ; i < linhas ; i++){
-        mapa[i] = malloc(sizeof(char)* (colunas+1));
+    m.matriz = malloc(sizeof(char*) * m.linhas);
+    for(i = 0 ; i < m.linhas ; i++){
+        m.matriz[i] = malloc(sizeof(char)* (m.colunas+1));
     }
 }
 
@@ -30,26 +29,69 @@ void lemapa(){
         exit(1);
     }
 
-    fscanf(f, "%d %d", &linhas, &colunas);
+    fscanf(f, "%d %d", &(m.linhas), &(m.colunas));
 
     alocamapa();
     
-    for(i = 0; i < linhas; i++){
-        fscanf(f, "%s", mapa[i]);
+    for(i = 0; i < m.linhas; i++){
+        fscanf(f, "%s", m.matriz[i]);
     }
     fclose(f);
 }
 
-int main(){
-    lemapa();
-    
-    mapa[0][0] = '|';
-    mapa[4][9] = '@';
+void imprimemapa(){
+        for(int i = 0; i < m.linhas; i++){
+        printf("%s\n", m.matriz[i]);
+        }
+}
 
-    int i;
-    for(i = 0; i < linhas; i++){
-        printf("%s\n", mapa[i]);
+int acabou(){
+    return 0;
+}
+
+void move(char direcao){
+    int x;
+    int y;
+    for(int i = 0; i < m.linhas; i++){
+        for(int j = 0; j < m.colunas; j++){
+            if(m.matriz[i][j] == '@'){
+                x = i;
+                y = j;
+                break;
+            }
+        }
     }
+    switch(direcao){
+        case 'a':
+            m.matriz[x][y-1] = '@';
+            break;
+        case 'w':
+            m.matriz[x-1][y] = '@';
+            break;
+        case 's':
+            m.matriz[x+1][y] = '@';
+            break;
+        case 'd':
+            m.matriz[x][y+1] = '@';            
+    }
+
+    m.matriz[x][y] = '.';
+}
+
+
+int main(){
+
+   lemapa();
+    
+    do
+    {
+        imprimemapa();
+
+        char comando;
+        scanf(" %c", &comando);
+        move(comando);
+    } while (!acabou());
+    
 
     liberamapa();
 
--- game.h
+++ game.h
@@ -1,3 +1,15 @@
 void liberamapa();
 void alocamapa();
 void lemapa();
+
+void move(char direcao);
+int acabou();
+void imprimemapa();
+
+struct mapa {
+    char** matriz;
+    int linhas;
+    int colunas; 
+};
+
+typedef struct mapa MAPA;
